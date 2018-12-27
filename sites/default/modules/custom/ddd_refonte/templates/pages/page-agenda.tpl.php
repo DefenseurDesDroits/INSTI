d3a9cba0782cf42612c249883395a9a04ae717ae
EmSAR <41960736+EmSAR@users.noreply.github.com>
2018-12-27 17:46:21 +0100

    Update page-agenda.tpl.php

--- sites/default/modules/custom/ddd_refonte/templates/pages/page-agenda.tpl.php
+++ sites/default/modules/custom/ddd_refonte/templates/pages/page-agenda.tpl.php
@@ -6,12 +6,12 @@
 <div class="agenda col-md-12">
 	<div class="prec-agenda col-xs-4 col-sm-4">
 		<a href="?month=<?php print $vars['prev_month']; ?>"><i class="fa fa-angle-left pict-agenda-left" aria-hidden="true"></i>
-		<p class="prec"><?php print t('Previous'); ?></p>
+		<p class="prec"><?php print t('Previous month'); ?></p>
 		</a>
 	</div>
 	<div class="prec-agenda col-xs-4 col-sm-4 col-xs-offset-4 col-sm-offset-4">
 		<a href="?month=<?php print $vars['next_month']; ?>"><i class="fa fa-angle-right pict-agenda-right" aria-hidden="true"></i>
-		<p class="prec"><?php print t('Next'); ?></p>
+		<p class="prec"><?php print t('Next month'); ?></p>
 		</a>
 	</div>
 </div>
