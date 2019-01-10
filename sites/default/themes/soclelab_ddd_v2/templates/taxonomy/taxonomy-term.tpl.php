05fea029e5db92416cfc6cd647e2b78c322a1069
EmSAR <41960736+EmSAR@users.noreply.github.com>
2019-01-10 16:57:02 +0100

    Update taxonomy-term.tpl.php

--- sites/default/themes/soclelab_ddd_v2/templates/taxonomy/taxonomy-term.tpl.php
+++ sites/default/themes/soclelab_ddd_v2/templates/taxonomy/taxonomy-term.tpl.php
@@ -88,47 +88,7 @@
   <?php if (!$page): ?>
     <h2><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
   <?php endif; ?>
-  <div class="row">
-	<div class="col-md-12">
-		<nav class="navbar navbar-default">
-			<div class="container-fluid">
-				<!-- Brand and toggle get grouped for better mobile display -->
-			    <div class="navbar-header">
-			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-102" aria-expanded="false">
-			        <span class="sr-only"></span>
-			        <i class="fa fa-chevron-right" aria-hidden="true"></i>
-			      </button>
-				             <?php foreach($vars['menu'] as $m) : ?>
-                                                  <?php if($m['active'] == TRUE) : ?>
-                                                          <p class="navbar-brand"><?=$m['name']?></p>
-                                                  <?php  endif; ?>
-                                                  <?php endforeach; ?>
-												  <!--
-			      <p class="navbar-brand"><?php print(t("All")); ?></p>
-				  -->
-			    </div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-102">
-					<ul class="nav navbar-nav menu-taxo-vues">
-					<?php foreach($vars['menu'] as $m) : ?>
-						<li role="presentation">
-							<a href="<?=$m['path']?>" <?php print ($m['active'] == TRUE ? 'class="active"' : '')?>><?=$m['name']?></a>
-						</li>
-					<?php endforeach; ?>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		
-	</div>
-</div>
-<div class="row">
-  <div class="col-md-12"><?php print render($vars['filter']); ?></div>
-</div>
-<div class="txt-resultats col-md-12"><?php print $vars['nb_result'] . t(' Result(s) for ') . '"' . $vars['name'] . '"'; ?></div>
-<?php print $vars['view']; ?>
-
-  <div class="content">
+   <div class="content">
     <?php print render($content); ?>
   </div>
 
