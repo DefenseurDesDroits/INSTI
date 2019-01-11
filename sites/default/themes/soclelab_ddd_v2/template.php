cceeb3f161fa446017f4f1c3c41364195c899905
EmSAR <41960736+EmSAR@users.noreply.github.com>
2019-01-11 15:47:22 +0100

    Revert "RAS"
    This reverts commit 04e672f689701e756af3ad62dc651b0f75dc72a4.

--- sites/default/themes/soclelab_ddd_v2/template.php
+++ sites/default/themes/soclelab_ddd_v2/template.php
@@ -150,6 +150,13 @@ function soclelab_ddd_v2_preprocess_taxonomy_term(&$variables) {
   if($variables['tid'] == '28' || $parent[0]->tid == '28'){
      $type_histoire = TRUE;
   }
+   /* Début Ajout MANU 1/3 */
+  /* $type_recrutement = FALSE;
+  if($variables['tid'] == '1583' || $parent[0]->tid == '1583'){
+     $actus = taxonomy_get_children('1583');
+	 $type_recrutement = TRUE;
+  }*/
+ /* Fin ajout MANU 1/3 */  
   if(check_taxonomy_actualites($actus, $variables['tid'])){
     $children = taxonomy_get_children($variables['tid']);
     $menu[0]['path'] = url('taxonomy/term/' . $variables['tid']);
@@ -168,7 +175,14 @@ function soclelab_ddd_v2_preprocess_taxonomy_term(&$variables) {
     if($type_histoire == TRUE){
       $variables['count'] = get_views_actualite('nodequeue_2', 'histoires_vecues', $variables['tid'])->total_rows;
       $variables['view_actualites'] = get_views_actualite('nodequeue_2', 'histoires_vecues', $variables['tid']);  
-    } else {
+    } 
+	   /* Début Ajout MANU 2/3 */
+	elseif( $type_recrutement == TRUE){
+		$variables['count'] = get_views_actualite('nous_rejoindre', 'block_3', $variables['tid'])->total_rows;
+      $variables['view_actualites'] = get_views_actualite('nous_rejoindre', 'block_3', $variables['tid']);  
+	}
+	   /* Fin Ajout MANU 2/3 */
+	else {
       $variables['view_actualites'] = get_views_actualite('vue_actualites', 'block_actu_v2', $variables['tid']);
     }
   }
@@ -194,7 +208,14 @@ function soclelab_ddd_v2_preprocess_taxonomy_term(&$variables) {
     if($type_histoire == TRUE){
       $variables['count'] = get_views_actualite('nodequeue_2', 'histoires_vecues', $variables['tid'])->total_rows;
       $variables['view_actualites'] = get_views_actualite('nodequeue_2', 'histoires_vecues', $variables['tid']);
-    } else {
+    }
+	   /* Début Ajout MANU 3/3 */
+	elseif( $type_recrutement == TRUE){
+		$variables['count'] = get_views_actualite('nous_rejoindre', 'block_3', $variables['tid'])->total_rows;
+      $variables['view_actualites'] = get_views_actualite('nous_rejoindre', 'block_3', $variables['tid']);  
+	}
+	   /* Début Ajout MANU 3/3 */
+	else {
       $variables['view_actualites'] = get_views_actualite('vue_actualites', 'block_actu_v2', $variables['tid']);
     }
   }
