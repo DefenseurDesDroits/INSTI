d6294f11301ba601423c824792a439bd1aef03ad
EmSAR <41960736+EmSAR@users.noreply.github.com>
2019-01-11 17:55:30 +0100

    Update facil-iti.js

--- sites/default/themes/soclelab_ddd_v2/js/facil-iti.js
+++ sites/default/themes/soclelab_ddd_v2/js/facil-iti.js
@@ -1,8 +1,6 @@
-<script type="text/javascript">
 (function(w, d, s, f) {
 w[f] = w[f] || {conf: function () { (w[f].data = w[f].data || []).push(arguments);}};
 var l = d.createElement(s), e = d.getElementsByTagName(s)[0];
 l.async = 1; l.src = 'https://ws.facil-iti.com/tag/faciliti-tag.min.js'; e.parentNode.insertBefore(l, e);
 }(window, document, 'script', 'FACIL_ITI'));
 FACIL_ITI.conf('userId', 'fcec6806-ec0d-11e8-9726-000c298ed446');
-</script>
